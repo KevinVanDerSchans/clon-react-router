9dbc5858ed76bbc194eaecb182a5bb5e30aa849d
Kevin Schans <kevinvdsd@hotmail.com>
2024-04-14 13:16:21 +0200

    Create Router component

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,30 +1,23 @@
 import HomePage from '../../pages/Home/HomePage'
 import AboutPage from '../../pages/About/AboutPage'
-import { useState, useEffect } from 'react'
-import { EVENTS } from '../../constants'
+import { Router } from '../../router/Router'
 import './App.css'
 
+const appRoutes = [
+  {
+    path: '/',
+    Component: HomePage,
+  },
+  {
+    path: '/about',
+    Component: AboutPage,
+  },
+]
+
 function App() {
-  const [currentPath, setCurrentPath] = useState(window.location.pathname)
-
-  useEffect(() => {
-    const onLocationChange = () => {
-      setCurrentPath(window.location.pathname)
-    }
-
-    window.addEventListener(EVENTS.PUSHSTATE, onLocationChange)
-    window.addEventListener(EVENTS.POPSTATE, onLocationChange)
-
-    return () => {
-      window.removeEventListener(EVENTS.PUSHSTATE, onLocationChange)
-      window.removeEventListener(EVENTS.POPSTATE, onLocationChange)
-    }
-  }, [])
-
   return (
     <main>
-      {currentPath === '/' && <HomePage />}
-      {currentPath === '/about' && <AboutPage />}
+      <Router routes={appRoutes} />
     </main>
   )
 }
