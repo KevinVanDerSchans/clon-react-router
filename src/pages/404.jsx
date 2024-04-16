5e60ded724eb34d0e636d328fb94b7535b79db7c
Kevin Schans <kevinvdsd@hotmail.com>
2024-04-17 00:14:17 +0200

    Adjust 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -3,7 +3,7 @@ import { Link } from '../components/Link/Link'
 export default function Page404() {
   return (
     <>
-      <h1>This is not fine</h1>
+      <h1>Error 404</h1>
 
       <Link to='/'>Back to Home</Link>
     </>
