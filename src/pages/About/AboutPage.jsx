2a3a14146c9af0627f7619b990b6b3c7cb036c9c
Kevin Schans <kevinvdsd@hotmail.com>
2024-04-16 21:36:24 +0200

    Add i18n configuration

--- src/pages/About/AboutPage.jsx
+++ src/pages/About/AboutPage.jsx
@@ -1,13 +1,31 @@
-import { navigate } from '../../components/Link/Link'
 import { Link } from '../../components/Link/Link'
 
-export default function AboutPage() {
+const i18n = {
+  es: {
+    title: 'Sobre nosotros',
+    description: 'Esto es un clon de React Router',
+    button: 'Ir a inicio',
+  },
+  en: {
+    title: 'About us',
+    description: 'This is a clone of React Router',
+    button: 'Go to Home',
+  },
+}
+
+const useI18n = (lang) => {
+  return i18n[lang] || i18n.en
+}
+
+export default function AboutPage({ routeParams }) {
+  const i18n = useI18n(routeParams.lang ?? 'en')
+
   return (
     <>
-      <h1>About</h1>
+      <h1>{i18n.title}</h1>
 
-      <p>About page example</p>
-      <Link to='/'>Go to Home</Link>
+      <p>{i18n.description}</p>
+      <Link to='/'>{i18n.button}</Link>
     </>
   )
 }
