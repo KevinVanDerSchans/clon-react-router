75e3d54785984305a4ebd3b7ec9900e189ce07f1
Kevin Schans <kevinvdsd@hotmail.com>
2024-04-16 02:49:55 +0200

    Create Route component

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -2,6 +2,7 @@ import HomePage from '../../pages/Home/HomePage'
 import AboutPage from '../../pages/About/AboutPage'
 import SearchPage from '../../pages/Search/SearchPage'
 import { Router } from '../../router/Router'
+import { Route } from '../Route/Route'
 import Page404 from '../../pages/404'
 import './App.css'
 
@@ -23,7 +24,10 @@ const appRoutes = [
 function App() {
   return (
     <main>
-      <Router routes={appRoutes} defaultComponent={Page404} />
+      <Router routes={appRoutes} defaultComponent={Page404}>
+        <Route path='/' Component={HomePage} />
+        <Route path='/about' Component={AboutPage} />
+      </Router>
     </main>
   )
 }
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,8 +1,8 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, Children } from 'react'
 import { EVENTS } from '../constants'
 import { match } from 'path-to-regexp'
 
-export function Router({ routes = [], defaultComponent: DefaultComponent = () => null }) {
+export function Router({ children, routes = [], defaultComponent: DefaultComponent = () => null }) {
   const [currentPath, setCurrentPath] = useState(window.location.pathname)
 
   useEffect(() => {
@@ -21,7 +21,17 @@ export function Router({ routes = [], defaultComponent: DefaultComponent = () =>
 
   let routeParams = {}
 
-  const Page = routes.find(({ path }) => {
+  // Add routes from children <Route /> components
+  const routesFromChildren = Children.map(children, ({ props, type }) => {
+    const { name } = type
+    const isRoute = name === 'Route'
+
+    return isRoute ? props : null
+  })
+
+  const routesToUse = routes.concat(routesFromChildren)
+
+  const Page = routesToUse.find(({ path }) => {
     if (path === currentPath) return true
 
     const matcherUrl = match(path, { decode: decodeURIComponent })
