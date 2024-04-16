d793dff5fb3ac62bde1ec642f64289d5f447667c
Kevin Schans <kevinvdsd@hotmail.com>
2024-04-16 23:31:22 +0200

    Finish Router test

--- src/router/Router.test.jsx
+++ src/router/Router.test.jsx
@@ -1,6 +1,13 @@
-import { describe, it, expect, beforeEach } from 'vitest'
-import { render, screen, cleanup } from '@testing-library/react'
+import { describe, it, expect, beforeEach, vi } from 'vitest'
+import { render, screen, cleanup, fireEvent } from '@testing-library/react'
 import { Router } from './Router'
+import { getCurrentPath } from '../utils/utils'
+import { Route } from '../components/Route/Route'
+import { Link } from '../components/Link/Link'
+
+vi.mock('../utils/utils.js', () => ({
+  getCurrentPath: vi.fn(),
+}))
 
 describe('Given the Router component', () => {
   beforeEach(() => {
@@ -18,6 +25,8 @@ describe('Given the Router component', () => {
   })
 
   it('Then it should render the component from the first matching path', () => {
+    getCurrentPath.mockReturnValue('/about')
+
     const routes = [
       {
         path: '/',
@@ -30,6 +39,34 @@ describe('Given the Router component', () => {
     ]
 
     render(<Router routes={routes} />)
-    expect(screen.getByText('Home')).toBeTruthy()
+    expect(screen.getByText('About')).toBeTruthy()
+  })
+
+  it('Then it should navigate using Links', async () => {
+    getCurrentPath.mockReturnValueOnce('/')
+
+    render(
+      <Router>
+        <Route
+          path='/'
+          Component={() => {
+            return (
+              <>
+                <h1>Home</h1>
+                <Link to='/about'>Go to About</Link>
+              </>
+            )
+          }}
+        />
+        <Route path='/about' Component={() => <h1>About</h1>} />
+      </Router>
+    )
+
+    const anchor = screen.getByText(/Go to About/)
+    fireEvent.click(anchor)
+
+    const aboutTitle = await screen.findByText('About')
+
+    expect(aboutTitle).toBeTruthy()
   })
 })
